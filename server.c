bd77edf7df4d87f408fc614f9049748d6b4ed4d0
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-11-22 16:56:03 +0900

    fix

--- server.c
+++ server.c
@@ -2,19 +2,21 @@
 #include <winsock2.h>
 
 int main(void) {
-  struct sockaddr_in dest;
-  memset(&dest, 0, sizeof(dest));
+  struct sockaddr_in dest1;
+  struct sockaddr_in dest2;
+  memset(&dest1, 0, sizeof(dest1));
+  memset(&dest2, 0, sizeof(dest2));
   
-  dest.sin_family = AF_INET;
-  dest.sin_port = htons(8080);
-  dest.sin_addr.s_addr = inet_addr("127.0.0.1");
+  dest1.sin_family = AF_INET;
+  dest1.sin_port = htons(8080);
+  dest1.sin_addr.s_addr = inet_addr("127.0.0.1");
 
   WSADATA data;
   WSAStartup(MAKEWORD(2, 0), &data);
 
   SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
 
-  if (connect(s, (struct sockaddr *) &dest, sizeof(dest)) != 0) {
+  if (connect(s, (struct sockaddr *) &dest1, sizeof(dest1)) != 0) {
     printf("接続に失敗しました\n");
     return -1;
   }
