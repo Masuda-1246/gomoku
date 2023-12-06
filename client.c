b4512d7092347561b5d5635ebe5ca90f0cc7c321
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-12-06 12:54:22 +0900

    fix

--- client.c
+++ client.c
@@ -48,8 +48,8 @@ int main(void) {
 
     char buffer[1024];
     char name[20];
-		char response[1024];
-		int bytes_received = 0;
+    char response[1024];
+    int bytes_received = 0;
     recv(s1, buffer, 1024, 0);
     printf("%s\n", buffer);
     scanf("%s", name);
@@ -57,7 +57,7 @@ int main(void) {
 
     while (1) {
         memset(&buffer, 0, sizeof(buffer));
-				memset(&response, 0, sizeof(response));
+		memset(&response, 0, sizeof(response));
         recv(s1, buffer, 1024, 0);
         printf("from server ---> %s\n", buffer);
         char msg[20];
--- server.c
+++ server.c
@@ -3,9 +3,7 @@
 
 int main(void) {
   struct sockaddr_in dest1;
-  struct sockaddr_in dest2;
   memset(&dest1, 0, sizeof(dest1));
-  memset(&dest2, 0, sizeof(dest2));
   
   dest1.sin_family = AF_INET;
   dest1.sin_port = htons(8080);
@@ -34,6 +32,24 @@ int main(void) {
   // レスポンスを受信
   char response[1024];
   int bytes_received = recv(s, response, 1024, 0);
+  response[bytes_received] = '\0';
+  x = 3;
+  y = 3;
+  sprintf(command, "%d,%d", x, y);
+  printf(response);
+
+  memset(&dest1, 0, sizeof(dest1));
+  s = socket(AF_INET, SOCK_STREAM, 0);
+  if (connect(s, (struct sockaddr *) &dest1, sizeof(dest1)) != 0) {
+    printf("接続に失敗しました\n");
+    return -1;
+  }
+
+  // コマンドを送信
+  send(s, command, strlen(command), 0);
+
+  // レスポンスを受信
+  bytes_received = recv(s, response, 1024, 0);
   response[bytes_received] = '\0';
 
   // レスポンス表示
